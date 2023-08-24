41177169521c14aaa8d3a8c5b9cf1737c5f005c1
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-23 20:18:32 -0700

    object skills

---
+++ object.js
@@ -0,0 +1,55 @@
+// # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+// let names = {
+//   first_name: "hayden",
+//   last_name: "shelley",
+//   email: "hayden@gmail.com",
+// };
+
+// console.log(names.first_name);
+// console.log(names.last_name);
+// console.log(names.email);
+
+// # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+// let people = [
+//   { first_name: "hayden", last_name: "shelley" },
+//   { first_name: "jaime", last_name: "miller" },
+//   { first_name: "brandon", last_name: "shelley" },
+// ];
+
+// console.log(`${people[0].first_name} ${people[0].last_name}`);
+// console.log(`${people[1].first_name} ${people[1].last_name}`);
+// console.log(`${people[2].first_name} ${people[2].last_name}`);
+
+// # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+// let prices = { shirt: 20.99, pants: 40.99, socks: 5.99 };
+
+// prices.hat = 14.99;
+
+// console.log(prices);
+
+// # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+// let book = {
+//   title: "Lord of the Rings",
+//   pages: 500,
+//   language: "english",
+// };
+
+// console.log(book.title);
+// console.log(book.pages);
+// console.log(book.language);
+
+// # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+// # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+
+// # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+// # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+// # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+// # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
