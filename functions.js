c2e74d163e0dee79a65b4c7902223cc03fd92d21
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-23 20:30:25 -0700

    functions practice

---
+++ functions.js
@@ -0,0 +1,79 @@
+// # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+// function timesTwo(num) {
+//   return num * 2;
+// }
+
+// console.log(timesTwo(5));
+
+// # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+// function makeCaps(string) {
+//   return string.toUpperCase();
+// }
+
+// console.log(makeCaps("hello"));
+
+// # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+// function subtract(numOne, numTwo) {
+//   return numOne - numTwo;
+// }
+
+// console.log(subtract(5, 3));
+
+// # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+// function timesItself(num) {
+//   return num * num;
+// }
+
+// console.log(timesItself(5));
+
+// # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+// function firstLetter(string) {
+//   return string[0];
+// }
+
+// console.log(firstLetter("hello"));
+
+// # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+// function combineStrings(stringOne, stringTwo, stringThree) {
+//   return `${stringOne} ${stringTwo} ${stringThree}`;
+// }
+
+// console.log(combineStrings("one", "two", "three"));
+
+// # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+// function toString(num) {
+//   return num.toString();
+// }
+
+// console.log(toString(5));
+
+// # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+// function fiveString(string) {
+//   return string.repeat(5);
+// }
+
+// console.log(fiveString("hello"));
+
+// # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+
+// function average(array) {
+//   let i = 0;
+//   let sum = 0;
+//   while (i < array.length) {
+//     sum += array[i];
+//     i++;
+//   }
+//   return sum / array.length;
+// }
+
+// console.log(average([5, 3, 5, 3, 66, 4, 2]));
+
+// # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result
